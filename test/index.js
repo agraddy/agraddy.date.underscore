5cd50483b85a192c5c8c80bc8cc88e1b0bbcccbf
Anthony Graddy <anthony@locationq.com>
2017-02-07 22:54:56 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+tap.assert.equal(mod(new Date('2016-01-02 10:11:12')), '2016_01_02_10_11_12', 'Should be equal.');
 
+tap.assert(mod(), 'Should return a result.');
 
